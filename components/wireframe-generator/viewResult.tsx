96fe8e99b2b7d5fd4b578c5e0d3c7cb8f6ac3182
ispasdani <ispasdani14@gmail.com>
2025-04-28 00:10:57 +0200

    deapendecies added for sandpack + basic setup

--- components/wireframe-generator/viewResult.tsx
+++ components/wireframe-generator/viewResult.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import { Sandpack } from "@codesandbox/sandpack-react";
+import { useCodeStore } from "@/store/codeStore";
+import { DEPENDENCY } from "@/consts/sandpackDependecy";
 
 const ViewResult = () => {
+  const codeData = useCodeStore((state) => state.codeData);
+
   return (
     <div>
       {/* Code Editor and Preview */}
-      <Sandpack />
+      <Sandpack
+        template="react"
+        options={{
+          externalResources: ["https://cdn.tailwindcss.com"],
+          showNavigator: true,
+          showTabs: true,
+          editorHeight: 500,
+        }}
+        customSetup={{
+          dependencies: {
+            ...DEPENDENCY,
+          },
+        }}
+        files={{
+          "/App.js": `export default function App() {
+    return <h1>Hello Sandpack</h1>
+  }`,
+        }}
+      />
     </div>
   );
 };
