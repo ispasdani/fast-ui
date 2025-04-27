d45030ac3f84412420e88be70e0805abc9395dba
ispasdani <ispasdani14@gmail.com>
2025-04-27 23:54:40 +0200

    sandpack by codebox editor added

--- components/wireframe-generator/viewResult.tsx
+++ components/wireframe-generator/viewResult.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Sandpack } from "@codesandbox/sandpack-react";
 
 const ViewResult = () => {
-  return <div>{/* Code Editor and Preview */}</div>;
+  return (
+    <div>
+      {/* Code Editor and Preview */}
+      <Sandpack />
+    </div>
+  );
 };
 
 export default ViewResult;
